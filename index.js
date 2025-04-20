3db2ccb8e06edff63f0d963e382cca0ea84148ed
Salem <salem.zakkar@unifi.solutions>
2025-04-20 21:33:54 +0300

    fix

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ const moviesRouter = require("./src/Routes/movies_route");
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log(req.url);
-  console.log(req.params)
-  console.log(req.body);
-  next();
-});
-
 app.use("/api/movies", moviesRouter);
+
+
 
 module.exports = app;
